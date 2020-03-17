721f69b0d3a3b75a0caa53083036255c81ebab5a
DiegoSaburi <50275371+DiegoSaburi@users.noreply.github.com>
2020-03-17 15:44:53 -0300

    Commit na feature pelo vs code

--- helloworld/src/helloworld/main.java
+++ helloworld/src/helloworld/main.java
@@ -9,14 +9,14 @@ public class main {
 		playlist minha_playlist = new playlist();
 		musica music1 = new musica("longas palavras",225,"Cu de cachorro");
 		musica music2 = new musica("as vezes",225,"quem");
-		musica music3 = new musica("batata",225,"cabeça");
+		musica music3 = new musica("batata",225,"cabeï¿½a");
 		
 		minha_playlist.append(music1);
 		minha_playlist.append(music2);
 		minha_playlist.append(music3);
 		
 		minha_playlist.print();
-		
+		//Comment de teste
 		minha_playlist.print();
 		
 	}
